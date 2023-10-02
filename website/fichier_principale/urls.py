b92d7d975f5d2b63b6c6245115e6414208b4a07c
root <root@Attito>
2023-10-02 08:26:57 +0000

    push

--- website/fichier_principale/urls.py
+++ website/fichier_principale/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('aide', views.aide, name='aide'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('information', views.information, name='information'),
-    path('service', views.service, name='service'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('information/', views.information, name='information'),
+    path('service/', views.service, name='service'),
 
 
-    path('impot', views.impot, name='impot'),
+    path('impot/', views.impot, name='impot'),
     
     
-    path('login', views.login, name='login'),
-    path('signup1', views.signup1, name='signup1'),
+    path('login/', views.login, name='login'),
+    path('signup1/', views.signup1, name='signup1'),
 ]
--- website/fichier_principale/views.py
+++ website/fichier_principale/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth.models import  User, auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User, auth
+
+from django.contrib import messages
 
 # definission des pages principale.
 
@@ -11,7 +14,7 @@ def aide(request):
     return render(request, "dossier_principale/aide.html", {})
 
 def dashboard(request):
-    return render(request, "dossier_principale/dashboard.html", {})
+    return render(request, "dossier_principale/dashbord.html", {})
 
 def information(request):
     return render(request, "dossier_principale/informations.html", {})
@@ -29,15 +32,18 @@ def impot(request):
 
 def login(request):
     if request.method =='POST':
-        email = request.POST['username']
-        password = request.POST['pawd']
-        user = auth.authenticate(username=username, password=pwd)
-
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
         if user is not None:
-            auth.login(request.user)
-            return render('dossier_principale/home.html')
+            auth.login(request, user)
+            return redirect('dossier_principale/home')
         else:
+            messages.error(request, 'Email ou mot de passe incorrect.')
             return render(request, "dossier_connexion/login.html", {})
+    else:
+        return render(request, "dossier_connexion/login.html", {})
+
 
 def signup1(request):
     if request.method == 'POST':
@@ -49,9 +55,11 @@ def signup1(request):
         # Vérifiez si un utilisateur avec le même nom d'utilisateur existe déjà
         if User.objects.filter(username=username).exists():
             # Gérez le cas où le nom d'utilisateur est déjà pris
+            messages.error(request, 'Nom déjà utilisé. Veuillez choisir un autre nom d\'utilisateur.', extra_tags='error')
             return render(request, 'dossier_principale/home.html')
         # Vérifiez si les mots de passe correspondent
         if pwd == pwd2:
+            messages.success(request, 'Compte créé avec succès. Vous pouvez maintenant vous connecter.', extra_tags='success')
             # Créez un nouvel utilisateur avec l'adresse e-mail comme nom d'utilisateur
             user = User.objects.create_user(username=username, email=email, password=pwd)
             # Mettez à jour le nom d'utilisateur avec la valeur capturée dans le formulaire
@@ -62,15 +70,11 @@ def signup1(request):
             return render(request, 'dossier_principale/home.html')
         else:
             # Gérez le cas où les mots de passe ne correspondent pas
+            # messages.error(request, 'Les mots de passe ne correspondent pas. Veuillez réessayer.', extra_tags='error')
             return render(request, 'dossier_connexion/signup1.html')
     else:
         # Gérez le cas où la méthode de la requête n'est pas POST
         return render(request, 'dossier_connexion/signup1.html')
 
-
-
-# def home(request):
-#     return render(request, "dossier_principale/home.html", {})
-
-# def home(request):
-#     return render(request, "dossier_principale/home.html", {})
+def logout(request):
+    return redirect('login')
